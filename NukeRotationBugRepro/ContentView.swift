ed10cee550ab9576497410bd030e32d5048ff987
Tom Brewe <tom@nylkiway.net>
2026-04-14 16:40:34 +0200

    Initial Commit

---
+++ NukeRotationBugRepro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NukeRotationBugRepro
+//
+//  Created by Tom Brewe on 14.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NukeRotationBugRepro/NukeRotationBugReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  NukeRotationBugReproApp.swift
+//  NukeRotationBugRepro
+//
+//  Created by Tom Brewe on 14.04.26.
+//
+
+import SwiftUI
+
+@main
+struct NukeRotationBugReproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
